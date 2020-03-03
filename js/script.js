5a8da8c918ac6097407129abef38fb65168f09fc
Julia Melnik <yuliyamielnik@gmail.com>
2020-03-03 20:34:03 +0100

    Add task 5.4 to blog

--- js/script.js
+++ js/script.js
@@ -28,7 +28,50 @@ function titleClickHandler(event) {
   targetArticle.classList.add('active');
 }
 
+
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+
+function generateTitleLinks() {
+  /*  [DONE]  remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+
+  /* [DONE] for each article */
+  const articles = document.querySelector(optArticleSelector);
+  console.log(articles);
+  let html = ''; /* O co chodzi??? */
+  for (let article of articles) {
+
+    console.log(article);
+
+    /* [DONE] get the article id */
+    const articleId = clickedElement.getAttribute('id');
+
+    /* [DONE] find the title element */ /*   get the title from the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML; /* o co chodzi z inner */
+    console.log(articleTitle);
+
+
+    /*  [DONE] create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML); /* jak robimy console
+
+ /* insert link into titleList */
+
+    html = html + linkHTML;
+    console.log(html);
+  }
+
+  titleList.innerHTML = html;
+}
 const links = document.querySelectorAll('.titles a');
+console.log(links);
 for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
+
+generateTitleLinks();
