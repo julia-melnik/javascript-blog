a7f13d8f7fe45faca3c6e7f194e24633aed73935
Julia Melnik <yuliyamielnik@gmail.com>
2020-03-05 09:11:58 +0100

    add o corrections

--- js/script.js
+++ js/script.js
@@ -2,19 +2,19 @@
 function titleClickHandler(event) {
   event.preventDefault();
   const clickedElement = this;
-  console.log('Link was clicked!'),
-  console.log(event);
 
-  /* [DONE] remove class 'active' from all article links  */
+
+
+  /*  remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* add class 'active' to the clicked link */
   clickedElement.classList.add('active');
 
-  /* [DONE]  remove class 'active' from all articles */
+  /*   remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post.active');
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
@@ -39,33 +39,33 @@ const optArticleSelector = '.post',
 
 
 function generateTitleLinks() {
-  /*  [DONE]  remove contents of titleList */
+  /*  remove contents of titleList */
+
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
 
-  /* [DONE] for each article */
-  const articles = document.querySelector(optArticleSelector);
+  /*  for each article */
+  const articles = document.querySelectorAll(optArticleSelector);
   console.log(articles);
   let html = ''; /* O co chodzi??? */
   for (let article of articles) {
 
-    /* [DONE] get the article id */
-    const clickedElement = this;
-    const articleId = clickedElement.getAttribute('id');
+    /*  get the article id */
+    const articleId = article.getAttribute('id');
 
-    /* [DONE] find the title element */ /*   get the title from the title element */
+    /* find the title element */ /*   get the title from the title element */
     const articleTitle = article.querySelector(optTitleSelector).innerHTML; /* o co chodzi z inner */
     console.log(articleTitle);
 
 
-    /*  [DONE] create HTML of the link */
+    /*  [ create HTML of the link */
     const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
     console.log(linkHTML); /* jak robimy console
 
  /* insert link into titleList */
 
     html = html + linkHTML;
-    console.log(html);
+
   }
 
   titleList.innerHTML = html;
